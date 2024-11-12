a82b918b9ff5e4d3cdfe4f49053b7b0d10113d37
Leonardo P. Monteiro <dev.pmonteiro@gmail.com>
2024-11-12 14:23:07 -0300

    daily commit

--- contact/urls.py
+++ contact/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from contact import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'contact'
 
 urlpatterns = [
     path('', views.index, name = 'index' ),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
